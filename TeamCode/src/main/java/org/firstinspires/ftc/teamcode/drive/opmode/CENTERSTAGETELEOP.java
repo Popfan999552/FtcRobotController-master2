f777354a3fa8b103dd7526bd0f680fa3c8d043ed
Popfan999552 <yimsoq@gmail.com>
2023-11-19 23:28:44 -0800

    Update CENTERSTAGETELEOP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CENTERSTAGETELEOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CENTERSTAGETELEOP.java
@@ -5,6 +5,7 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -16,16 +17,62 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class CENTERSTAGETELEOP extends OpMode {
     Gamepad previousGamepad;
     Gamepad currentGamepad;
+    //Drivetrain motors
     SampleMecanumDrive drive;
+
+    //Outtake motors
+    DcMotor slides;
+    DcMotor futureVirtualFourBar;
+    //Intake motors
+    DcMotor roller;
+    //endgame motor
+    DcMotor futureLeadScrew;
+    DcMotor winch1;
+    DcMotor winch2;
+    //Outtake servos
+    Servo leftServo;
+    Servo rightServo;
+    CRServo bucketServo;
+    //endgame servo
+    Servo droneLauncher;
+    CRServo scissorLift;
+
+
     Trajectory forward;
     Trajectory backward;
     Trajectory left;
     Trajectory right;
     @Override
     public void init() {
-        drive = new SampleMecanumDrive(hardwareMap);
         previousGamepad=gamepad1;
         currentGamepad=gamepad1;
+        //Drivetrain
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        //Outtake motors
+        slides = hardwareMap.dcMotor.get("slides");
+        //futureVirtualFourBar = hardwareMap.dcMotor.get("futureVirtualFourBar");
+
+        //Outtake servos
+        leftServo = hardwareMap.servo.get("leftServo");
+        rightServo = hardwareMap.servo.get("rightServo");
+        bucketServo = hardwareMap.crservo.get("bucketServo");
+
+        //endgame servo
+        droneLauncher = hardwareMap.servo.get("droneLauncher");
+        scissorLift = hardwareMap.crservo.get("scissorLift");
+
+        //Intake motors
+        roller = hardwareMap.dcMotor.get("roller");
+
+        //endgame motor
+        //futureLeadScrew = hardwareMap.dcMotor.get("futureLeadScrew");
+        winch1 = hardwareMap.dcMotor.get("winch1");
+        winch2 = hardwareMap.dcMotor.get("winch2");
+
+
+
+
 
         forward = drive.trajectoryBuilder(new Pose2d())
                 .forward(5)
@@ -40,14 +87,77 @@ public class CENTERSTAGETELEOP extends OpMode {
                 .strafeLeft(5)
                 .build();
 
-
-
     }
 
     @Override
     public void loop() {
         previousGamepad=currentGamepad;
         currentGamepad=gamepad1;
+
+        //intake controls
+        roller.setPower(currentGamepad.right_trigger);
+
+        //outtake controls
+        if(currentGamepad.dpad_up){
+            slides.setPower(1);
+        } else{
+            slides.setPower(0);
+        }
+        if(currentGamepad.dpad_down){
+            slides.setPower(-1);
+        } else{
+            slides.setPower(0);
+        }
+        rightServo.setPosition(currentGamepad.left_trigger);
+        leftServo.setPosition(currentGamepad.left_trigger);
+        if(currentGamepad.dpad_right){
+            bucketServo.setPower(1);
+        } else if(currentGamepad.dpad_left){
+            bucketServo.setPower(-1);
+        } else {
+            bucketServo.setPower(0);
+        }
+
+        //endgame controls
+        //hang
+        if(currentGamepad.b){
+            winch1.setPower(1);
+            winch2.setPower(1);
+            double x=getRuntime();
+            while(getRuntime()<getRuntime()+500){
+
+            }
+            scissorLift.setPower(1);
+            x=getRuntime();
+            while(getRuntime()<getRuntime()+500){
+
+            }
+            winch2.setPower(0);
+            winch1.setPower(0);
+            scissorLift.setPower(0);
+        } else if(currentGamepad.a){
+            scissorLift.setPower(-1);
+            double x=getRuntime();
+            while(getRuntime()<getRuntime()+500){
+
+            }
+            winch1.setPower(1);
+            winch2.setPower(1);
+            x=getRuntime();
+            while(getRuntime()<getRuntime()+500){
+
+            }
+        }
+
+        if(currentGamepad.x){
+            //release
+            droneLauncher.setPosition(0.7);
+        }
+        if(currentGamepad.y){
+            //retract servo
+            droneLauncher.setPosition(0.7);
+        }
+        //Gamepad1: drive, hanging
 
 /*
         if(!previousGamepad.a && currentGamepad.a){
